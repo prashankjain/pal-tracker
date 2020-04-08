0dcf4fb0c70e21a463288f2585a3edd2c5acd2b9
prashankjain <prashankjain@gmail.com>
2020-04-08 09:21:27 +0000

    Added Health and info

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -12,6 +12,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Pomelo.EntityFrameworkCore.MySql;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
 
 namespace PalTracker
 {
@@ -42,6 +46,10 @@ namespace PalTracker
 
             //services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
             services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+            services.AddCloudFoundryActuators(Configuration);
+            services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
+            services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+            services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +66,7 @@ namespace PalTracker
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseCloudFoundryActuators();
         }
     }
 }
